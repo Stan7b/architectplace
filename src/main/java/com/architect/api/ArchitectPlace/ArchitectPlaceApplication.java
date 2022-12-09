ecbf4693b682daeef5f1d34f6d429888f878be8d
stan7b <coolgamer12611@gmail.com>
2022-12-09 21:39:12 +0200

    first commit

---
+++ src/main/java/com/architect/api/ArchitectPlace/ArchitectPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.architect.api.ArchitectPlace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectPlaceApplication.class, args);
+	}
+
+}
